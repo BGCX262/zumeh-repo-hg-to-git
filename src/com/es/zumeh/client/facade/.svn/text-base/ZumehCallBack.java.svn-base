3646c42d69fd73049a7d1aa3c0b8300e3f063d67
htrmeira <htrmeira@gmail.com>
2011-11-02 21:40:56 -0300

    removing unused files

--- src/com/es/zumeh/client/facade/.svn/text-base/ZumehCallBack.java.svn-base
+++
@@ -1,5 +0,0 @@
-package com.es.zumeh.client.facade;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public abstract class ZumehCallBack implements AsyncCallback<String> {
-}
--- zumeh/src/com/es/zumeh/client/view/pages/NodeClickNotifier.java
+++
@@ -1,5 +0,0 @@
-package com.es.zumeh.client.view.pages;
-public interface NodeClickNotifier {
-  public void addClickListener(NodeClickListener listener);
-  public void removeClickListener(NodeClickListener listener);
-}
